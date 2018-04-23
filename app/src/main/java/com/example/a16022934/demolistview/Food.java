7b5a679e9d8ffccfd0b762492b439153ceab59c1
Javier <16022934@myrp.edu.sg>
2018-04-23 10:15:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022934/demolistview/Food.java
@@ -0,0 +1,20 @@
+package com.example.a16022934.demolistview;
+
+public class Food {
+    private String name;
+    private boolean star;
+
+    public Food(String name, boolean star) {
+        this.name = name;
+        this.star = star;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isStar() {
+        return star;
+    }
+}
+
